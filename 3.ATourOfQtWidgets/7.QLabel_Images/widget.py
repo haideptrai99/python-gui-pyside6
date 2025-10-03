770414da4c5327b6a0cbedfaa3305bbcbe2bb72e
haideptrai99 <haidaica99999@gmail.com>
2025-10-03 18:56:06 +0700

    7.QLabel_Images

---
+++ 3.ATourOfQtWidgets/7.QLabel_Images/widget.py
@@ -0,0 +1,17 @@
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("QLabel Image Demo")
+
+        image_label = QLabel()
+        image_label.setPixmap(QPixmap("images/minions.png"))
+
+        layout = QVBoxLayout()
+        layout.addWidget(image_label)
+
+        self.setLayout(layout)
